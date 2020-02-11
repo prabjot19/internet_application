6c8a44dbf54a770ab8b3ccda905124856908be8c
prabjot19 <prabjotram@gmail.com>
2020-02-11 19:00:41 +0000

    Lab 2 completed

--- node_intro/lib/messages.js
+++ node_intro/lib/messages.js
@@ -4,20 +4,42 @@ module.exports = function(url,callback){
   const mongoose = require('mongoose');
   mongoose.connect(url,callback);
 
+  const messageSchema = new mongoose.Schema(
+    {
+      username:{
+        type:String,
+        required:true
+      },
+      text:{
+        type:String,
+        required:true
+      }
+    },
+    {strict:'throw'}
+  );
   const Message = mongoose.model(
     'messages',
-    {username : String, text : String}
+    messageSchema
   );
   return {
     create:function(newMessage,callback){
-		var message = new Message(newMessage);
-		message.save(callback);
-      
+        try {
+          var message = new Message(newMessage);
+        } catch(exception) {
+          return callback('Unable to create Message');
+        }
+        if(message.username)
+          message.username = sanitizeHTML(message.username);
+        if(message.text)
+          message.text     = sanitizeHTML(message.text);
+        message.save(callback);
     },
     read:function(id,callback){
 		Message.findById(id,callback);
     },
     readUsername:function(username,callback){
+        if(typeof username !== 'string')
+          return callback('Unable to parse username.');
        Message.find({username:username},callback);
     },
     readAll:function(callback){
